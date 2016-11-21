5f7219fdaac9122f004a42315847f4340006ce2e
Michael Granados <michaelgranados@gmail.com>
2016-11-20 22:39:57 -0200

    compiling javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 const data = {
   stylus: {
@@ -11,6 +13,14 @@ const data = {
       compress: true,
       'include css': true
     }
+  },
+  javascript: {
+    src: ['./src/js/main.js'],
+    dest: {
+      path: './public/js/',
+      file: 'main.js'
+    },
+    watch: './src/js/*'
   }
 };
 
@@ -26,6 +36,19 @@ gulp.task('stylus:watch', ['stylus'], () => {
   return gulp.watch(data.stylus.watch, ['stylus']);
 });
 
-gulp.task('compile', ['stylus']);
+gulp.task('javascript', () => {
+  return gulp.src(data.javascript.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat(data.javascript.dest.file))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(data.javascript.dest.path));
+});
 
-gulp.task('default', ['stylus:watch']);
+gulp.task('javascript:watch', ['javascript'], () => {
+  return gulp.watch(data.javascript.watch, ['javascript']);
+});
+
+gulp.task('compile', ['stylus', 'javascript']);
+
+gulp.task('default', ['stylus:watch', 'javascript:watch']);
